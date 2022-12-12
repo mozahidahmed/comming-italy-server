73e4a93244dce7bdcd7533eb3ee7f5b8d672fdeb
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-12-12 15:04:56 +0600

    cc

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 //......................................................
 
- const uri=`mongodb+srv://cityitaly:FmcUZGB0NRWjbeMP@cluster0.unjca9b.mongodb.net/?retryWrites=true&w=majority`;
+ const uri="mongodb+srv://cityitaly:FmcUZGB0NRWjbeMP@cluster0.unjca9b.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
